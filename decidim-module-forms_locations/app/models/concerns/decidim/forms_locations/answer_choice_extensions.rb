ec6843d515cc0ce6e3c643a6a56135ad580da562
Joonas <joonas.aapro@mainiotech.fi>
2024-03-21 08:44:00 +0200

    Add optionality to answer_option

--- decidim-module-forms_locations/app/models/concerns/decidim/forms_locations/answer_choice_extensions.rb
+++ decidim-module-forms_locations/app/models/concerns/decidim/forms_locations/answer_choice_extensions.rb
@@ -6,15 +6,24 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
-        belongs_to :location_option,
-                   class_name: "LocationOption",
-                   foreign_key: "decidim_location_option_id",
-                   optional: true
+        # Define a module variable to track if the association has been defined
+        class_attribute :answer_option_association_defined
+        self.answer_option_association_defined = false unless self.answer_option_association_defined
 
-        belongs_to :answer_option,
-                   class_name: "AnswerOption",
-                   foreign_key: "decidim_answer_option_id",
-                   optional: true
+        # Redefine the belongs_to :answer_option association if not already defined
+        unless self.answer_option_association_defined
+          belongs_to :answer_option,
+                    class_name: "AnswerOption",
+                    foreign_key: "decidim_answer_option_id",
+                    optional: true
+
+          self.answer_option_association_defined = true
+        end
+
+        belongs_to :location_option,
+                  class_name: "LocationOption",
+                  foreign_key: "decidim_location_option_id",
+                  optional: true
       end
     end
   end
