9487c8d42241329b4544cdfba4ecc867e5227e0c
Joonas <joonas.aapro@mainiotech.fi>
2026-08-17 09:38:27 +0300

    Delete deprecated language change select

--- decidim-module-forms_locations/app/packs/src/decidim/forms/admin/forms.js
+++ decidim-module-forms_locations/app/packs/src/decidim/forms/admin/forms.js
@@ -9,7 +9,6 @@ import AutoButtonsByPositionComponent from "src/decidim/admin/auto_buttons_by_po
 import AutoLabelByPositionComponent from "src/decidim/admin/auto_label_by_position.component"
 import createDynamicFields from "src/decidim/admin/dynamic_fields.component"
 import createFieldDependentInputs from "src/decidim/admin/field_dependent_inputs.component"
-import initLanguageChangeSelect from "src/decidim/admin/choose_language"
 import sortable from "html5sortable/dist/html5sortable.es"
 
 export default function createEditableForm() {
@@ -659,7 +658,12 @@ export default function createEditableForm() {
       autoLabelByPosition.run();
       autoButtonsByPosition.run();
 
-      initLanguageChangeSelect($field.find("select.language-change").toArray());
+      const fieldElement = $field[0];
+      if (fieldElement) {
+        fieldElement.querySelectorAll("select.language-change").forEach((container) => {
+          window.deprecate(container, "language-change", "select.language-change")
+        });
+      }
 
       const sortableContainer = document.querySelector(".questionnaire-questions-list[data-draggable-table]");
       if (sortableContainer) {
